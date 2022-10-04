4aa5581472f1f07d4bddd17ab0f9f9158df2e8e6
khue.pd <@khue.pd>
2022-10-04 09:26:15 +0700

    BackEnd

---
+++ src/controller/product.js
@@ -0,0 +1,128 @@
+const Product = require("../models/product");
+const shortid = require("shortid");
+const slugify = require("slugify");
+const Category = require("../models/category");
+
+exports.createProduct = (req, res) => {
+  const { name, price, description, category, quantity, createdBy } = req.body;
+  let productPictures = [];
+
+  if (req.files.length > 0) {
+    productPictures = req.files.map((file) => {
+      return { img: file.filename };
+    });
+  }
+
+  const product = new Product({
+    name: name,
+    slug: slugify(name),
+    price,
+    quantity,
+    description,
+    productPictures,
+    category,
+    createdBy: req.user._id,
+  });
+
+  product.save((error, product) => {
+    if (error) return res.status(400).json({ error });
+    if (product) {
+      res.status(201).json({ product, files: req.files });
+    }
+  });
+};
+
+exports.getProductsBySlug = (req, res) => {
+  const { slug } = req.params;
+  Category.findOne({ slug: slug })
+    .select("_id type")
+    .exec((error, category) => {
+      if (error) {
+        return res.status(400).json({ error });
+      }
+
+      if (category) {
+        Product.find({ category: category._id }).exec((error, products) => {
+          if (error) {
+            return res.status(400).json({ error });
+          }
+
+          if (category.type) {
+            if (products.length > 0) {
+              res.status(200).json({
+                products,
+                priceRange: {
+                  under100k: 100000,
+                  under200k: 200000,
+                  under300k: 300000,
+                  under400k: 400000,
+                  under500k: 500000,
+                },
+                productsByPrice: {
+                  under100k: products.filter(
+                    (product) => product.price <= 100000
+                  ),
+                  under200k: products.filter(
+                    (product) =>
+                      product.price > 100000 && product.price <= 200000
+                  ),
+                  under300k: products.filter(
+                    (product) =>
+                      product.price > 200000 && product.price <= 300000
+                  ),
+                  under400k: products.filter(
+                    (product) =>
+                      product.price > 300000 && product.price <= 400000
+                  ),
+                  under500k: products.filter(
+                    (product) =>
+                      product.price > 400000 && product.price <= 500000
+                  ),
+                },
+              });
+            }
+          } else {
+            res.status(200).json({ products });
+          }
+        });
+      }
+    });
+};
+
+exports.getProductDetailsById = (req, res) => {
+  const { productId } = req.params;
+  if (productId) {
+    Product.findOne({ _id: productId }).exec((error, product) => {
+      if (error) return res.status(400).json({ error });
+      if (product) {
+        res.status(200).json({ product });
+      }
+    });
+  } else {
+    return res.status(400).json({ error: "Params required" });
+  }
+};
+
+// new update
+exports.deleteProductById = (req, res) => {
+  const { productId } = req.body.payload;
+  if (productId) {
+    Product.deleteOne({ _id: productId }).exec((error, result) => {
+      if (error) return res.status(400).json({ error });
+      if (result) {
+        res.status(202).json({ result });
+      }
+    });
+  } else {
+    res.status(400).json({ error: "Params required" });
+  }
+};
+
+exports.getProducts = async (req, res) => {
+  const products = await Product.find({ createdBy: req.user._id })
+    .select("_id name price quantity slug description productPictures category")
+    .populate({ path: "category", select: "_id name" })
+    .exec();
+
+  res.status(200).json({ products });
+};
